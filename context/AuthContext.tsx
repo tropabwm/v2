eb2a0107d4c5219104139647696cb38a6e9cca47
tropabwm <tropabwm@gmail.com>
2025-05-15 15:35:11 -0300

    Update AuthContext.tsx

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -47,7 +47,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [authToken, setAuthToken] = useState<string | null>(null);
   const router = useRouter();
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://v2-production-cc95.up.railway.app';
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://usbmktprov1.up.railway.app/';
 
   const processAndSetToken = useCallback((token: string | null): boolean => {
     if (token) {
